a7c75918f74769c8fee463bbbd248952c19cfa12
Matt Eva <matteweva@gmail.com>
2023-01-24 19:44:23 -0800

    added folders for base components

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -2,7 +2,14 @@ import styles from "./Sidebar.module.css"
 
 function Sidebar() {
   return (
-    <div className={styles.sidebar}>Sidebar</div>
+    <div className={styles.sidebar}>
+      <ul>
+        <li>My Writing</li>
+        <li>My List</li>
+        <li>My Library</li>
+        <li>Liked Writing</li>
+      </ul>
+    </div>
   )
 }
 
