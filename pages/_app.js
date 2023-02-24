b62fa3f77495ff5c20cdf775a29d455e7a06ffee
Matt Eva <matteweva@gmail.com>
2023-02-23 17:29:34 -0800

    set up authorize handling in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,24 @@
 import '../styles/globals.css'
 import UserContextProvider from '../context/user_context'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
+import { UserContext } from '../context/user_context'
 
 export default function App({ Component, pageProps }) {
-
+  const [user, setUser] = useState(null)
+  console.log(user)
   useEffect(() =>{
     console.log('running')
     async function getMe(){
       try {
         const res = await fetch('http://localhost:4000/me', {credentials: 'include'})
-        const data = await res.json()
-        console.log(data)
+        if (res.ok){
+          const data = await res.json()
+          setUser(data)
+        }else{
+          const error = await res.json()
+          console.log(error)
+        }
+
       } catch (error){
         console.log(error)
       }
@@ -19,8 +27,8 @@ export default function App({ Component, pageProps }) {
   }, [])
 
   return (
-    <UserContextProvider>
+    <UserContext.Provider value={{user, setUser}}>
       <Component {...pageProps} />
-    </UserContextProvider>
+    </UserContext.Provider>
   )
 }
