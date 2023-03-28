79171b3d251f06b213da5a908f61999ab114551f
Matt Eva <matteweva@gmail.com>
2023-03-27 18:57:26 -0700

    installed React Quill

--- components/WritingForm/WritingForm.js
+++ components/WritingForm/WritingForm.js
@@ -1,14 +1,22 @@
 import styles from "./WritingForm.module.css"
+import { useState } from "react"
 
 function WritingForm() {
+    const [formInput, setFormInput] = useState()
 
     const handleSelection = (e) =>{
-        console.log(document.getSelection().anchorNode.parentNode)
+        console.log(document.getSelection().anchorNode)
     }
+
+    const handleChange = (e) =>{
+        console.log(e.target.childNodes)
+        console.log(e.target.innerHTML)
+       
+    }
+
   return (
     <div>
-        <div contentEditable={true} className={styles.writingForm} onSelect={handleSelection}>
-
+        <div contentEditable={true} className={styles.writingForm} onInput={handleChange} onSelect={handleSelection}>
         </div>
     </div>
   )
